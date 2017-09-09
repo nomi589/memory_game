f2730987d6976c32daa225162779afb62ca108a6
Noman Rauf <nomi589@hotmail.com>
2017-09-09 16:18:38 +0300

    Completed

---
+++ README.md
@@ -0,0 +1,31 @@
+# Memory Game Project
+
+## Table of Contents
+
+* [Introduction](#introduction)
+* [How to Play](#how-to-play)
+* [Star-rating](#star-rating)
+* [Dependencies](#dependencies)
+
+## Introduction
+
+A simple memory game. Match cards in pairs. The game is completed when all cards are matched.
+
+## How to Play
+
+1. Click on a card to reveal it's face.
+2. Click on another card; if the two cards match, they remain face up. If they don't match, both cards turn face down.
+3. Each time two cards are turned over, one move is recorded.
+4. Match all 8 pairs to complete the game.
+5. Once all the 8 pairs of cards have been matched, the final star-rating is shown alongwith the number of moves and the amount of time it took to complete the game.
+6. The challenge is to keep the positions of the cards in your memory so the game is completed in fewer moves.
+
+## Star-rating
+
+Each game starts with 3 stars. Every time 12 moves are consumed, the number of stars is reduced by one.
+
+So if the game is completed in less than 12 moves, you get 3 stars. If it is completed in less than 24 moves, you get 2 stars and so on.
+
+## Dependencies
+
+This game requires a web browser with Javascript support and a working internet connection to run.
---
+++ js/app.js
@@ -0,0 +1,169 @@
+let cards = [
+    "fa fa-diamond", "fa fa-diamond",
+    "fa fa-paper-plane-o", "fa fa-paper-plane-o",
+    "fa fa-anchor", "fa fa-anchor",
+    "fa fa-bolt", "fa fa-bolt",
+    "fa fa-cube", "fa fa-cube",
+    "fa fa-leaf", "fa fa-leaf",
+    "fa fa-bicycle", "fa fa-bicycle",
+    "fa fa-bomb", "fa fa-bomb"
+];
+
+let deck = $('.deck');
+let movesDisplay = $('.moves');
+let starsDisplay = $('.stars');
+let restartButton = $('.restart');
+
+let openCards, matchCount, movesCount, startTime, timerId;
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function start() {
+    openCards = [];
+    matchCount = 0;
+    movesCount = 0;
+
+    movesDisplay.text(movesCount);
+    deck.children().remove();
+    starsDisplay.children().remove();
+
+    for (let i = 0; i < 3; i++) {
+        starsDisplay.append('<li><i class="fa fa-star"><i></li>');
+    }
+
+    cards = shuffle(cards);
+    for (let card of cards) {
+        deck.append('<li class="card"><i class="' + card + '"></i></li>');
+    }
+
+    startTime = new Date();
+    timerId = setInterval(updateTime, 100);
+}
+
+function showClickedCard(card) {
+    card.addClass('show open');
+}
+
+function addCardToOpenCardsList(card) {
+    openCards.push(card);
+}
+
+function openCardsMatch() {
+    let card1 = openCards[0].children().attr('class');
+    let card2 = openCards[1].children().attr('class');
+
+    return card1 === card2;
+}
+
+function lockOpenCards() {
+    for (card of openCards) {
+        card.removeClass('open');
+        card.addClass('match');
+    }
+
+    openCards = [];
+}
+
+function hideOpenCards() {
+    for (card of openCards) {
+        card.removeClass('open');
+        card.removeClass('show');
+    }
+    
+    openCards = [];
+}
+
+function updateMovesCountAndDisplay() {
+    movesCount++;
+    movesDisplay.text(movesCount);
+}
+
+function updateStarsDisplay() {
+    if (!(movesCount % 12)) {
+        starsDisplay.children().last().remove();
+    }
+}
+
+function allCardsMatched() {
+    return (matchCount == 8);
+}
+
+function displayFinalScore() {
+    let finalStars = '';
+    let finalStarsCount = starsDisplay.children().length;
+
+    for (let i = 0; i < finalStarsCount; i++) {
+        finalStars += '<i class="fa fa-star"></i>';
+    }
+
+    $('.final-stars').html(finalStars);
+    $('.movesCount').text(movesCount);
+    $('.final-time').text($('.elapsed-time').text());
+    $('.modal').css('display', 'flex');
+}
+
+function updateTime() {
+    $('.elapsed-time').text((Date.now() - startTime) / 1000);
+}
+
+deck.on('click', 'li', function(e) {
+    // Ignore the clicks on already matched cards
+    if (!($(this).hasClass('match') || $(this).hasClass('show'))) {
+        showClickedCard($(this));
+        addCardToOpenCardsList($(this));
+
+        if (openCards.length == 2) {
+            if (openCardsMatch()) {
+                lockOpenCards();
+                matchCount++;
+            } else {
+                setTimeout(hideOpenCards, 300);
+            }
+
+            updateMovesCountAndDisplay();
+            updateStarsDisplay();
+        }
+
+        if (allCardsMatched()) {
+            clearInterval(timerId);
+            displayFinalScore();
+        }
+    }
+});
+
+restartButton.click(function() { 
+    clearInterval(timerId);
+    start();
+});
+
+$('.close-modal-button').click(function() {
+    $('.final-stars').children().remove();
+    $('.modal').css('display', 'none');
+    start();
+});
+
+start();
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
